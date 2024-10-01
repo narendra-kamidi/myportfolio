8b0de139534fb1add64048a1426b8fda0b9c2126
narendra-kamidi <narendrakamidi842@gmail.com>
2024-10-01 10:37:16 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,8 @@
+var typed=new Typed('#element',{
+    strings: ['Web Developer','Devops Engineer','Software Tester'],
+    typeSpeed:100,
+    backSpeed:50,
+    backDelay:1,
+    loop:true,
+    
+});
