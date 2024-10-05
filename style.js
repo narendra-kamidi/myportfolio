39b45d417ca42815f9ef63c331770e4fec4ffd11
narendra-kamidi <narendrakamidi842@gmail.com>
2024-10-05 09:38:32 +0530

    Update style.js

--- style.js
+++ style.js
@@ -6,3 +6,18 @@ var typed=new Typed('#element',{
     loop:true,
     
 });
+
+function changeTextColor(){
+    const textElement=document.getElementById("randomtext");
+    const randomColor=getRandomColor();
+    textElement.style.color=randomColor;
+}
+function getRandomColor(){
+    const letters="0123456789ABCDEF";
+    let color="#";
+    for(let i=0;i<6;i++){
+        color+=letters[Math.floor(Math.random()*16)];
+    }
+    return color;
+}
+window.onload=changeTextColor;
